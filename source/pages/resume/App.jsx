c627c11fc46b7a7fc66ee70b30b758fe3f6995e7
hansin <huxinghan@loonxi.com>
2020-04-20 19:40:10 +0800

    add resume

--- source/pages/resume/App.jsx
+++ source/pages/resume/App.jsx
@@ -14,12 +14,68 @@ class App extends React.Component {
     location: PropTypes.object,
   }
 
-  pathName = null
+  baseInfo = [
+    {
+      t: '电话',
+      v: '18667933320',
+    },
+    {
+      t: '邮箱',
+      v: 'hansincn@gmail.com',
+    },
+    {
+      t: '学历',
+      v: '本科',
+    },
+    {
+      t: '居住地',
+      v: '杭州',
+    },
+    {
+      t: '生日',
+      v: '1991/02/02',
+    },
+  ]
+
+  title = (t) => {
+    return <div className={styles.title}>
+      <span>{t}</span>
+    </div>
+  }
 
   render () {
     return (
       <div className={`${styles.app}`}>
-        <div className={styles.resume}>sssssss</div>
+        <div className={styles.resume}>
+          <div className={styles.name}>Hansin</div>
+          <div className={styles.info}>
+            {
+              this.baseInfo.map(info => {
+                return <span key={info.t}>
+                  {`${info.t}: `}
+                  {
+                    info.link ? <a
+                      href={info.link}
+                      rel="noopener noreferrer"
+                      target="_blank"
+                    >{info.v}</a> : info.v
+                  }
+                </span>
+              })
+            }
+          </div>
+          <div>{this.title('自我概述')}</div>
+          <div className={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, culpa? Commodi iste velit alias cum dignissimos libero itaque eos expedita numquam, natus porro facere fugit inventore, placeat provident recusandae vero?</div>
+          <div>{this.title('专业技能')}</div>
+          <div className={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, culpa? Commodi iste velit alias cum dignissimos libero itaque eos expedita numquam, natus porro facere fugit inventore, placeat provident recusandae vero?</div>
+          <div>{this.title('工作经历')}</div>
+          <div className={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, culpa? Commodi iste velit alias cum dignissimos libero itaque eos expedita numquam, natus porro facere fugit inventore, placeat provident recusandae vero?</div>
+          <div>{this.title('教育背景')}</div>
+          <div className={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, culpa? Commodi iste velit alias cum dignissimos libero itaque eos expedita numquam, natus porro facere fugit inventore, placeat provident recusandae vero?</div>
+          <div>{this.title('项目经历')}</div>
+          <div className={styles.content}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel, culpa? Commodi iste velit alias cum dignissimos libero itaque eos expedita numquam, natus porro facere fugit inventore, placeat provident recusandae vero?</div>
+
+        </div>
       </div>
     )
   }
